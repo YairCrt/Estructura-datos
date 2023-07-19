391f56ac4f4b79b714dca7ff0b2bef4af138700a
CorYb <y-crbnz25@outlook.com>
2023-07-19 17:12:38 -0600

    Ejemplo de recursividad con serie de Fibonacci

---
+++ Recursion1/Recursion2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Recursion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 0;
+            Console.Write("Ingrese un numero para realizar su serie de Fibonacci: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            f = Fibonacci(num);
+
+            Console.Write("Fibonacci de {0}: {1}", num, f);
+
+            Console.Read();
+        }
+
+        public static int Fibonacci(int num)
+        {
+            int r = 0;
+            //Caso inductivo
+            if(num > 1)
+            {
+                r = Fibonacci(num - 1) + Fibonacci(num - 2);
+            }
+            //Caso base
+            if(num <= 1)
+            {
+                r = 1;
+            }
+            return r;
+        }
+    }
+
+}
