9fa9d632a3ff4d410a133f7b1949e0b957632965
CorYb <y-crbnz25@outlook.com>
2023-07-29 14:10:35 -0600

    ejemplo de fila/Queue

---
+++ Queue11/Queue11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue fila = new Queue();
+
+            fila.Enqueue(5);
+            fila.Enqueue(3);
+            fila.Enqueue(7);
+            fila.Enqueue(1);
+
+            fila.Transversa();
+
+            int valor = fila.Dequeue();
+            Console.WriteLine("Valor adquirido {0}", valor);
+            fila.Transversa();
+
+            fila.Enqueue(8);
+            fila.Transversa();
+
+            Console.WriteLine("Primer elemento de la fila: {0}", fila.Peek());
+
+            Console.Read();
+        }
+    }
+}
---
+++ Queue11/Queue11/Queue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue11
+{
+    internal class Queue
+    {
+        Nodo ancla;
+
+        Nodo trabajo;
+
+        public Queue()
+        {
+            ancla = new Nodo();
+
+            //Al ser una cola vacio su siguiente es null
+            ancla.Siguiente = null;
+        }
+
+        //Recorriendo todo el queue
+        public void Transversa()
+        {
+            trabajo = ancla;
+
+            //Se recorre hasta encontrar el final
+            while(trabajo.Siguiente != null)
+            {
+                trabajo = trabajo.Siguiente;
+
+                int d = trabajo.Dato;
+
+                Console.Write("<-{0} ", d);
+            }
+            Console.WriteLine();
+
+        }
+
+        public void Enqueue(int pDato)
+        {
+            trabajo = ancla;
+
+            //Se recorre hasta encontrar el final
+            while(trabajo.Siguiente != null) 
+            {
+                //Avanza trabajo
+                trabajo = trabajo.Siguiente;
+            }
+
+            //Se crea el nuevo nodo
+            Nodo temp = new Nodo();
+
+            //Inserta el dato
+            temp.Dato = pDato;
+
+            temp.Siguiente = null;
+
+            //se liga el utlimo nodo encontrado con el recien creado 
+            trabajo.Siguiente = temp;
+        }
+
+        public int Dequeue()
+        {
+            int valor = 0;
+
+            //Solo si hay elementos en el queue
+            if(ancla.Siguiente != null)
+            {
+                //Se obtiene el dato correspondiente
+                trabajo = ancla.Siguiente;
+                valor = trabajo.Dato;
+
+                //Se saca del queue
+                ancla.Siguiente = trabajo.Siguiente;
+                trabajo.Siguiente = null;
+            }
+            return valor;
+        }
+
+        public int Peek()
+        {
+            int valor = 0;
+
+            if( ancla.Siguiente != null)
+            {
+                trabajo = ancla.Siguiente;
+                valor = trabajo.Dato;
+            }
+            return valor;
+        }
+
+    }
+}
