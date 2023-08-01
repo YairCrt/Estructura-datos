6fe1749571bf1b8bb133429b9211927ed73d8a70
CorYb <y-crbnz25@outlook.com>
2023-08-01 16:49:58 -0600

    Se agrega ejemplo de arbol binario de busqueda

---
+++ ArbolBinario/ArbolBinario/ArbolBB.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    internal class ArbolBB
+    {
+        private Nodo raiz;
+        private Nodo trabajo;
+
+        private int i = 0;
+
+        public ArbolBB()
+        {
+            raiz = null;
+        }
+
+        internal Nodo Raiz { get; set; }
+
+        //Insertar(metodo recursivo)
+        public Nodo Insertar(int pDato, Nodo pNodo)
+        {
+            Nodo temp = null;
+
+            //Si no hay a quien insertar entonces creamos el nodo
+            if(pNodo == null)
+            {
+                temp = new Nodo();
+                temp.Dato = pDato;
+
+                return temp;
+            }
+
+            if(pDato < pNodo.Dato)
+            {
+                pNodo.Izq = Insertar(pDato, pNodo.Izq);
+            }
+            if(pDato > pNodo.Dato)
+            {
+                pNodo.Der = Insertar(pDato, pNodo.Der);
+            }
+
+            return pNodo;
+        }
+
+        //Transversa
+        public void Transversa(Nodo pNodo)
+        {
+            if (pNodo == null)
+                return;
+
+            //Se procesa la raiz
+            for(int n = 0; n < i; n++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(pNodo.Dato);
+
+            //Si se tiene izq, se procesa el lado izq
+            if(pNodo.Izq != null)
+            {
+                i++;
+                Console.Write("I ");
+                Transversa(pNodo.Izq);
+                i--;
+            }
+
+            //Si se tiene der, se procesa el lado der
+            if(pNodo.Der != null)
+            {
+                i++;
+                Console.Write("D ");
+                Transversa(pNodo.Der);
+                i--;
+            }
+        }
+
+    }
+
+
+}
---
+++ ArbolBinario/ArbolBinario/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolBB arbol = new ArbolBB();
+
+            Nodo raiz = arbol.Insertar(6, null); //Null para que sea el nodo raiz
+            arbol.Insertar(2, raiz);
+            arbol.Insertar(8, raiz);
+            arbol.Insertar(1, raiz);
+            arbol.Insertar(4, raiz);
+            arbol.Insertar(3, raiz);
+            arbol.Insertar(5, raiz);
+            arbol.Insertar(7, raiz);
+            arbol.Insertar(11, raiz);
+            arbol.Insertar(9, raiz);
+            arbol.Insertar(10, raiz);
+            arbol.Insertar(0, raiz);
+            arbol.Insertar(-1, raiz);
+            arbol.Insertar(12, raiz);
+            arbol.Insertar(14, raiz);
+
+            arbol.Transversa(raiz);
+
+
+            Console.ReadLine();
+        }
+    }
+}
